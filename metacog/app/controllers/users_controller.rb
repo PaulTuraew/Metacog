610da1037b0eb9e059e27313b1d95b2eb8bc2e7e
paulturaew <paul.turaew@gmail.com>
2014-10-27 18:06:57 -0400

    create user works and sends to show.html.erb

--- metacog/app/controllers/users_controller.rb
+++ metacog/app/controllers/users_controller.rb
@@ -8,4 +8,27 @@ def new
   @user = User.new
 end
 
-end
+def create
+    @user = User.new(users_params)
+    if @user.save
+      redirect_to @user #goes to user - show.html.erb
+    else
+      render :new    #rails will look in views folder for new.html.erb
+    end
+  end
+
+  def show
+  end
+
+private
+
+def users_params
+    params.require(:user).permit(
+      :first_name,
+      :last_name,
+      :password,
+      :password_confirmation,
+      :phone_num,
+      )
+  end
+end #ends class
--- metacog/db/schema.rb
+++ metacog/db/schema.rb
@@ -11,9 +11,45 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20141027203643) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "companies", force: true do |t|
+    t.string   "co_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "industries", force: true do |t|
+    t.string   "sector"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "moods", force: true do |t|
+    t.integer  "rating"
+    t.string   "reason"
+    t.text     "narrative"
+    t.datetime "date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.integer  "phone_num"
+    t.string   "remember_token"
+    t.boolean  "admin",           default: false, null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token", using: :btree
 
 end
